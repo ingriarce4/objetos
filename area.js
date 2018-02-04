42bd5a0b14096c3774cbfd0ac4eacbc855e548e4
Susana Arce <ingridarce4@hotmail.com>
2018-02-03 21:05:23 -0300

    cambios class objetos

--- area.js
+++ area.js
@@ -2,7 +2,7 @@
 /*2.- definir tipo de clase o prototipos que existe en js para 
 definir los puntos 
 */
-function Punto (x, y){
+/*function Punto (x, y){
 this.x = x
 this.y = y
 }
@@ -19,9 +19,32 @@ const x = this.x - p.x
 const y = this.y - p.y
 return Math.sqrt(x*x + y*y) 
 }
+*/
 
-const p1 = new Punto(0,4)
-const p2 = new Punto(3,0)
+/*2.- manera de crear objetos*/
+// crear un nuevo objeto Punto, antes era una funcioin
+/*const Punto = {
+init: function init(x, y) {
+this.x = x
+this.y = x
+ },
+ moverEnX: function moverEnX(x){
+ this.x += x
+ },
+ moverEnY: function moverEnY(y){
+ this.y +=  y
+ },
+ distancia: function distancia(p){
+  const x = this.x - p.x
+  const y = this.y - p.y
+return Math.sqrt(x*x + y*y) 
+ }
+}
+
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
+p1.init (0,4)
+p2.init (3,0)
 
 
 //se le ponen atributos
@@ -30,5 +53,36 @@ const p2 = new Punto(3,0)
  p1.moverEnX(10)
  console.log (p1. distancia(p2))
  p2.moverEnX(-4) 
- console.log (p1. distancia(p2))
+ console.log (p1. distancia(p2))*/
  
+/*
+3.- manera de crear objetos*/
+
+
+class Punto {
+	constructor (x,y){
+	this.x = x
+	this.y = y
+  }
+   moverEnX (x){
+	this.x += x
+  }
+    moverEnY (y){
+	this.x += y
+  }
+	distancia (p){
+  const x = this.x - p.x
+  const y = this.y - p.y
+  return Math.sqrt(x*x + y*y)
+	}
+ }
+
+const p1 = new Punto (0,4)
+const p2 = new Punto (3,0)
+//se le ponen atributos
+ console.log (p1.distancia(p2))
+ console.log (p2.distancia(p1))
+ p1.moverEnX(10)
+ console.log (p1.distancia(p2))
+ p2.moverEnX(-4) 
+ console.log (p1.distancia(p2))
